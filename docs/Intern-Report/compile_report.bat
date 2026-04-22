993c301d0bc0587bf86e5534d3c979652da4f41c
Rakesh Biswas <rbiswas01999@gmail.com>
2026-04-22 21:03:53 +0600

    feat: initialize intern report documentation and LaTeX template files

---
+++ docs/Intern-Report/compile_report.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Compiling VerbaSense Internship Report...
+
+:: First pass to generate aux files
+pdflatex -interaction=nonstopmode report.tex
+
+:: Process bibliography
+bibtex report
+
+:: Second pass to include citations
+pdflatex -interaction=nonstopmode report.tex
+
+:: Third pass to finalize cross-references
+pdflatex -interaction=nonstopmode report.tex
+
+echo.
+echo Compilation complete!
+echo Cleaning up auxiliary files...
+
+del *.aux *.log *.out *.toc *.blg *.bbl
+
+echo Done.
+pause
